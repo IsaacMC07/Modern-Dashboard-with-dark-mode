0df0edee77cb39691a982686872414085431b92c
IsaacMC07 <148183307+IsaacMC07@users.noreply.github.com>
2025-06-04 16:09:06 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+//Modern-Dashboard-with-dark-mode
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Modern-Dashboard-with-dark-mode/',
   plugins: [react()],
 })
